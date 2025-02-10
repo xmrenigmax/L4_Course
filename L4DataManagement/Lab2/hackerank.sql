9b38f9190da30ca6c0fe476c1bfb451e917d1d90
xmrenigmax <rileyjordan21@hotmail.com>
2025-02-10 11:38:56 +0000

    Refactor city table script to use MERGE for data insertion and updates; add selection queries for city names

--- L4DataManagement/Lab2/hackerank.sql
+++ L4DataManagement/Lab2/hackerank.sql
@@ -1,8 +1,9 @@
 -- Date: 2025/02/10
 -- Author: Riley Jordan
+-- Description: This script creates a table called city and inserts values into the table. It then selects all from the city table and prints the name of all selected cities.
 
--- Drops the table city if it exists so that we can create a new one
-DROP TABLE CITY
+-- Drop the city table if it exists
+DROP TABLE CITY;
 -- Creates a table called city
 CREATE TABLE CITY
 (
@@ -13,10 +14,27 @@ CREATE TABLE CITY
     POPULATION NUMBER
 );
 
--- Inserts values into the city table
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (3793, 'Kabul', 'AFG', 'Kabol', 1780000);
+-- Merge the city tables data if it exists
+    --CITY c stands for the city table "c" is the temporary name for the table
+MERGE INTO CITY c
+    -- selects values from city table as c and values selected from source table (src)
+USING (SELECT 3793 AS ID, 'Kabul' AS NAME, 'AFG' AS COUNTRYCODE, 'Kabol' AS DISTRICT, 1780000 AS POPULATION FROM DUAL) src
+    -- ON = join condition, c.ID = src.ID is the condition for the join
+ON (c.ID = src.ID)
+    -- if condition match, update city table with values from src
+WHEN MATCHED THEN
+    UPDATE SET c.NAME = src.NAME, c.COUNTRYCODE = src.COUNTRYCODE, c.DISTRICT = src.DISTRICT, c.POPULATION = src.POPULATION
+    -- if condition does not match, insert values into city table
+WHEN NOT MATCHED THEN
+    INSERT (ID,Name,COUNTRYCODE,DISTRICT,POPULATION) 
+    VALUES (src.ID, src.NAME, src.COUNTRYCODE, src.DISTRICT, src.POPULATION);
 
 -- select all from the city table
 SELECT * FROM CITY;
 
 -- prints the name of all selected cities
+
+SELECT table_name FROM user_tables;
+
+-- new 
+SELECT NAME FROM CITY WHERE MOD(2,0) = 0;
